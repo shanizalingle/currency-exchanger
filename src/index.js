b245c1ba41d9b411bc5be95ce1441e9e05c7864d
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-09 16:33:54 -0700

    add reponse for 404 error

--- src/index.js
+++ src/index.js
@@ -19,11 +19,15 @@ function getCurrency(baseCurrency, newCurrency, amount) {
 // UI Logic
 
 function printElements(response, baseCurrency, newCurrency, amount) {
-  document.querySelector("p#output").innerText = `${amount} ${baseCurrency} in ${newCurrency} is ${response.conversion_result}`;
+    document.querySelector("p#output").innerText = `${amount} ${baseCurrency} in ${newCurrency} is ${response.conversion_result}`;
 }
 
 function printError(error, baseCurrency, newCurrency) {
-  document.querySelector("p#output").innerText = `There was an error accessing the currency data for ${baseCurrency} or ${newCurrency}: ${error}.`;
+  if (error.toString().includes("404")) {
+    document.querySelector("p#output").innerText = `${baseCurrency} or ${newCurrency} don't exist: ${error}`
+  } else {
+    document.querySelector("p#output").innerText = `There was an error accessing the currency data for ${baseCurrency} or ${newCurrency}: ${error}`;
+  }
 }
 
 function handleFormSubmission(event) {
@@ -31,7 +35,6 @@ function handleFormSubmission(event) {
   const baseCurrency = document.querySelector("input#base-currency-input").value;
   const newCurrency = document.querySelector("input#new-currency-input").value;
   const amount = document.querySelector("input#amount-input").value;
-  document.querySelector("input#new-currency-input").value = null;
   getCurrency(baseCurrency, newCurrency, amount);
   console.log(`amount: ${amount}`);
   console.log(`base currency:  ${baseCurrency}`);
